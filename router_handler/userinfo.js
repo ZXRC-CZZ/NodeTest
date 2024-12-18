b5449209db4b75b5bddfda49040bf7022d779117
ZXRC-CZZ <III0201@163.com>
2024-12-18 20:34:56 +0800

    fix:跟新密码 有报错问题 但不影响修改   更新头像也完成了

--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -2,6 +2,10 @@
 const db = require('../db/index')
 
 
+// 在头部区域导入 bcryptjs 后，
+// 即可使用 bcrypt.compareSync(提交的密码，数据库中的密码) 方法验证密码是否正确
+// compareSync() 函数的返回值为布尔值，true 表示密码正确，false 表示密码错误
+const bcrypt = require('bcryptjs')
 
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
@@ -37,4 +41,52 @@ exports.updateUserInfo = (req, res) => {
         // 修改用户信息成功
         return res.cc('修改用户基本信息成功！', 0,true)
       })
+  }
+
+
+  //修改密码// 更新用户密码的处理函数
+exports.updatePassword = (req, res) => {
+    // 根据 id 查询用户的信息
+    const sql = `select * from ev_users where id=?`
+    // 执行根据 id 查询用户的信息的 SQL 语句
+    db.query(sql, req.user.id, (err, results) => {
+      // 执行 SQL 语句失败
+      if (err) return res.cc(err)
+      // 判断结果是否存在
+      if (results.length !== 1) return res.cc('用户不存在！')
+  
+      // 判断密码是否正确
+      const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].password)
+      if (!compareResult) return res.cc('旧密码错误！')
+  
+      // 定义更新密码的 SQL 语句
+      const sql1 = `update ev_users set password=? where id=?`
+      // 对新密码进行加密处理
+      const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+      // 调用 db.query() 执行 SQL 语句
+      db.query(sql1, [newPwd, req.user.id], (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+        // 判断影响的行数
+        if (results.affectedRows !== 1) return res.cc('更新密码失败！')
+        // 成功
+        res.cc('更新密码成功', 0,ture)
+      })
+    })
+  }
+
+
+  // 更新用户头像的处理函数
+exports.updateAvatar = (req, res) => {
+const sql = 'update ev_users set user_pic=? where id=?'
+db.query(sql, [req.body.avatar, req.user.id], (err, results) => {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+  
+    // 执行 SQL 语句成功，但是影响行数不等于 1
+    if (results.affectedRows !== 1) return res.cc('更新头像失败！')
+  
+    // 更新用户头像成功
+    return res.cc('更新头像成功！', 0,true)
+  })
   }
